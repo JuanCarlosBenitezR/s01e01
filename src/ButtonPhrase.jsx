5ec3150b2a6e7230a4e60ac19c31825565e64395
JuanCarlosBenitezR <juan_carlos_2609@hotmail.com>
2025-04-28 21:51:13 -0600

    changeTitleClass

--- src/ButtonPhrase.jsx
+++ src/ButtonPhrase.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const ButtonPhrase = ({ onUpdatePhrase }) => {
-	console.log('Clic a hijo');
 	return (
 		<button
 			className="mb-10  hover:bg-red-700 text-black-400 hover:text-amber-400 rounded-md "
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Title = () => {
+function Title() {
 	return (
 		<div>
 			<h1 className="text-center font-serif font-extrabold text-indigo-800 italic shadow-lg tracking-wide animate-bounce absolute top-1/10 left-1/2 transform -translate-x-1/2 text-balance text-lg sm:text-[2.9 rem] md:text-3xl lg:text-4xl xl: text-7xl 2xl: text-9xl  ">
@@ -8,6 +8,6 @@ const Title = () => {
 			</h1>
 		</div>
 	);
-};
+}
 
 export default Title;
