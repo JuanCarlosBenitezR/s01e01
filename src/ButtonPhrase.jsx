ad2ba3632ca765cbd86b0ab6f20868c33a53f158
JuanCarlosBenitezR <juan_carlos_2609@hotmail.com>
2025-04-29 00:18:02 -0600

    OrderCode

--- src/ButtonPhrase.jsx
+++ src/ButtonPhrase.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const ButtonPhrase = ({ onUpdatePhrase }) => {
 	return (
 		<button
-			className="font-bold rounded-2xl h-fit w-fit mx-auto mb-2 px-3 text-base md:text-2xl lg:text-4xl xl:text-5xl bg-red-600 hover:bg-amber-300 text-amber-300 hover:text-red-600 active:bg-green-500"
+			className="font-bold rounded-2xl h-fit w-fit mx-auto mb-2 px-3 text-base md:text-2xl lg:text-4xl
+       xl:text-5xl bg-red-600 hover:bg-amber-300 text-amber-300 hover:text-red-600 active:bg-green-500"
 			onClick={onUpdatePhrase}
 		>
 			Ver otra frase
